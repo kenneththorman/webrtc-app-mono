3d219d46fdd6c4b635bbddfd956c27a273c44578
Kenneth Thorman <kenneth.thorman@appinux.com>
2013-12-21 16:48:58 +0100

    Adding Java Webrtc original source code

---
+++ WebRtc.Mono.Droid/src/video_engine/test/android/gen/R.java.d
@@ -0,0 +1,11 @@
+/home/user1/WebRTCDemo/trunk/webrtc/video_engine/test/android/gen/org/webrtc/videoengineapp/R.java \
+ : /home/user1/WebRTCDemo/trunk/webrtc/video_engine/test/android/res/drawable/logo.png \
+/home/user1/WebRTCDemo/trunk/webrtc/video_engine/test/android/res/values/strings.xml \
+/home/user1/WebRTCDemo/trunk/webrtc/video_engine/test/android/res/layout/aconfig.xml \
+/home/user1/WebRTCDemo/trunk/webrtc/video_engine/test/android/res/layout/main.xml \
+/home/user1/WebRTCDemo/trunk/webrtc/video_engine/test/android/res/layout/tabhost.xml \
+/home/user1/WebRTCDemo/trunk/webrtc/video_engine/test/android/res/layout/vconfig.xml \
+/home/user1/WebRTCDemo/trunk/webrtc/video_engine/test/android/res/layout/row.xml \
+/home/user1/WebRTCDemo/trunk/webrtc/video_engine/test/android/res/layout/send.xml \
+/home/user1/WebRTCDemo/trunk/webrtc/video_engine/test/android/res/layout/both.xml \
+/home/user1/WebRTCDemo/trunk/webrtc/video_engine/test/android/bin/AndroidManifest.xml \
