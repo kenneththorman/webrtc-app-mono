b7dd84719f8f3f9d550b2ba92ddd2725f8df4891
Kenneth Thorman <kenneth.thorman@appinux.com>
2013-12-23 10:53:30 +0100

    Renaming the Java Binding Library
    - renaming the java binding library to match the actual package name in
    java for consistency
    - adding the jar in the java bindings library also to the app as per the
    answer found here in the first answer
    (http://stackoverflow.com/questions/10911214/monodroid-issue-with-jar-binding-zubhium-sdk)

--- Org.Webrtc.Videoengineapp/Properties/AssemblyInfo.cs
+++ Org.Webrtc.Videoengineapp/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using Android.App;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WebRtc.Mono.ViEAndroidJavaAPI")]
+[assembly: AssemblyTitle("Org.Webrtc.Videoengineapp")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WebRtc.Mono.ViEAndroidJavaAPI")]
+[assembly: AssemblyProduct("Org.Webrtc.Videoengineapp")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
